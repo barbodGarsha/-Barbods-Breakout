c744e6ee8861bd5e6b6aa18cc8924553cd94b156
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-11-16 14:49:59 +0100

    fixed a little bug

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-	public float paddle_speed = 0.5f;
+	public float paddle_speed;
 	private Vector3 paddle_pos = new Vector3(0, -4.5f, 0);
 
 	// Update is called once per frame
@@ -12,7 +12,7 @@ public class Paddle : MonoBehaviour
 	{
 		//We can move the paddle with arrow keys now
 		//TODO: Add moving with mouse... it's hard to play with the Arrow keys
-		float x_pos = transform.position.x + (Input.GetAxis("Horizontal") * paddle_speed);
+		float x_pos = transform.position.x + (Input.GetAxis("Horizontal") * paddle_speed * Time.deltaTime);
 
 		//TODO: after designing a level the max and min value in Mathf.Clamp() function needs to change
 		paddle_pos = new Vector3(Mathf.Clamp(x_pos, -8f, 8f), paddle_pos.y, 0);
