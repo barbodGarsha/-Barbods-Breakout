669ea070adafcb5b3d4c5b6079155c351f1e54b7
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-11-17 12:21:50 +0100

    added a simple brick that gets destroyed by the ball

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+	//a simple Brick that gets destroyed by the ball
+    void OnCollisionEnter2D(Collision2D col)
+	{
+        if (col.gameObject.tag == "Ball")
+        {
+            Destroy(gameObject);
+            //TODO: Score system
+            //TODO: animations and sounds
+        }
+	}
+}
