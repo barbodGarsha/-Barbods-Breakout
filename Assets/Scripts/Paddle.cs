bc72f284bc8fa3d222c0b4968851008b17294f05
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-11-17 15:04:04 +0100

    added Moving with the mouse disabled moving with keyboard for now

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,7 +7,8 @@ public class Paddle : MonoBehaviour
 	const float LEFT_MAX = -9.7f;
 	const float RIGHT_MAX = 9.7f;
 
-	public float paddle_speed = 35;
+	//Note: this is only needed for the Moving with the keyboard
+	//public float paddle_speed = 35;
 
 	private Vector2 paddle_pos;
     private void Start()
@@ -15,17 +16,33 @@ public class Paddle : MonoBehaviour
 		paddle_pos = this.transform.position;
     }
 
+
     // Update is called once per frame
     void Update()
 	{
-		//We can move the paddle with arrow keys now
-		//TODO: Add moving with mouse... it's hard to play with the Arrow keys
-		float x_pos = transform.position.x + (Input.GetAxis("Horizontal") * paddle_speed * Time.deltaTime);
+		Vector3 mouse_pos = Input.mousePosition;
+		// Get distance the paddle is in front of the camera
+		mouse_pos.z = Mathf.Abs(transform.position.z - Camera.main.transform.position.z);
+		mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
 
 		//TODO: after designing a level the max and min value in Mathf.Clamp() function needs to change
-		paddle_pos = new Vector2(Mathf.Clamp(x_pos, LEFT_MAX, RIGHT_MAX), paddle_pos.y);
+		paddle_pos = new Vector2(Mathf.Clamp(mouse_pos.x, LEFT_MAX, RIGHT_MAX), paddle_pos.y);
 
 		//Move the paddle
 		transform.position = paddle_pos;
+
+		//----------------------------------------
+		// Moving the paddle with kyboard 
+		////We can move the paddle with arrow keys now
+		////TODO: Add moving with mouse... it's hard to play with the Arrow keys
+		//float x_pos = transform.position.x + (Input.GetAxis("Horizontal") * paddle_speed * Time.deltaTime);
+
+		////TODO: after designing a level the max and min value in Mathf.Clamp() function needs to change
+		//paddle_pos = new Vector2(Mathf.Clamp(x_pos, LEFT_MAX, RIGHT_MAX), paddle_pos.y);
+
+		////Move the paddle
+		//transform.position = paddle_pos;
+		//----------------------------------------
+
 	}
 }
