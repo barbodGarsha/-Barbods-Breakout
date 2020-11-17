4542cef674e61ec0fe36fe4fb3daf4cc993ac162
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-11-17 11:56:38 +0100

    cleaned the code a little

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-	public float paddle_speed;
-	private Vector3 paddle_pos = new Vector3(0, -4.5f, 0);
+	const float LEFT_MAX = -8f;
+	const float RIGHT_MAX = 8f;
+
+	public float paddle_speed = 35;
+	private Vector2 paddle_pos = new Vector2(0, -4.5f);
 
 	// Update is called once per frame
 	void Update()
@@ -15,7 +18,7 @@ public class Paddle : MonoBehaviour
 		float x_pos = transform.position.x + (Input.GetAxis("Horizontal") * paddle_speed * Time.deltaTime);
 
 		//TODO: after designing a level the max and min value in Mathf.Clamp() function needs to change
-		paddle_pos = new Vector3(Mathf.Clamp(x_pos, -8f, 8f), paddle_pos.y, 0);
+		paddle_pos = new Vector2(Mathf.Clamp(x_pos, LEFT_MAX, RIGHT_MAX), paddle_pos.y);
 
 		//Move the paddle
 		transform.position = paddle_pos;
