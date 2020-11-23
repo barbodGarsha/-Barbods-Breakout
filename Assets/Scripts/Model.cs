64a913baf2e998b967df76229a7a7296c2d35cb1
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-11-23 21:36:31 +0100

    tried to implement MVC but it's not finished

---
+++ Assets/Scripts/Model.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model : MonoBehaviour
+{
+
+    //Game 
+    public enum status
+    {
+        PLAYING,
+        PAUSE,
+        WON,
+        GAMEOVER,
+        MENU
+    }
+
+    public status game_status = status.PLAYING;
+    public int bricks;
+    public int lives = 1;
+    int score = 0;
+
+    //Clouds
+    public const float CLOUD_SPEED = 0.65f;
+   // public Vector3 clouds_start_pos; ??
+}
